728dcedf2d6b1f7dbe6d33281ddf6bb7804f9ca3
sasilab <sasistartup@gmail.com>
2025-06-22 13:30:59 +0200

    new file modification with new function added

--- status.py
+++ status.py
@@ -6,3 +6,27 @@ def status():
     """
     return "Application is running smoothly."
 
+
+class Status:
+    """
+    A class to represent the status of the application.
+    """
+
+    def __init__(self):
+        self.message = "Application is running smoothly."
+
+    def get_status(self):
+        """
+        Returns the status message.
+        """
+        return self.message
+
+def main():
+    """
+    Main function to demonstrate the status functionality.
+    """
+    print(status())
+    app_status = Status()
+    print(app_status.get_status())  
+
+    
